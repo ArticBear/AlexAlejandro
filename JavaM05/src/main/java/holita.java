4768f694a172675d48e987a31384a52036a69ce1
Alex Marín <alejandro.marin@pistacero.net>
2022-02-06 00:32:23 +0100

    Subiendo elefantes a git

--- JavaM05/src/main/java/holita.java
+++ JavaM05/src/main/java/holita.java
@@ -5,8 +5,8 @@ public class holita {
         System.out.println("Hola Alejandro!!! soy Alex!!");
 
         //iniciamos rama desarrollo
-        for (int i = 0; i < 15; i++) {
-            System.out.println("PiM PaM");
+        for (int i = 1; i < 15; i++) {
+            System.out.println(i + "elefante"+(i>1?"s":"")+" se balanceaba sobre la tela de una araña!");
         }
     }
 }
