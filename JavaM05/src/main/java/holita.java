701cca915d965b91d655480eb9f4f8c31766c4d9
Alex Marín <alejandro.marin@pistacero.net>
2022-02-06 00:15:57 +0100

    Iniciando rama Desarrollo

--- JavaM05/src/main/java/holita.java
+++ JavaM05/src/main/java/holita.java
@@ -3,5 +3,10 @@ public class holita {
     public static void main(String ... args){
         System.out.println("Hola Mundo!!! soy Alejandro!!");
         System.out.println("Hola Alejandro!!! soy Alex!!");
+
+        //iniciamos rama desarrollo
+        for (int i = 0; i < 15; i++) {
+            System.out.println("PiM PaM");
+        }
     }
 }
