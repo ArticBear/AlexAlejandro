c803be0548015effd78062affe1248068d4680a3
AlexMacias <incremented@outlook.es>
2022-02-05 23:41:02 +0100

    confirmar en "main"

--- JavaM05/src/main/java/holita.java
+++ JavaM05/src/main/java/holita.java
@@ -2,5 +2,6 @@ public class holita {
 
     public static void main(String ... args){
         System.out.println("Hola Mundo!!! soy Alejandro!!");
+        System.out.println("Hola Alejandro!!! soy Alex!!");
     }
 }
